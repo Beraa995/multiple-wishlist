c91a9049bffd70508cf6acd10b68f9f67fe48e92
Berin <berin@web-vision.de>
2020-11-06 20:17:20 +0100

    Add module's required files

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @category  BKozlic
+ * @package   BKozlic\MultipleWishlist
+ * @author    Berin Kozlic - berin.kozlic@gmail.com
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'BKozlic_MultipleWishlist', __DIR__);
