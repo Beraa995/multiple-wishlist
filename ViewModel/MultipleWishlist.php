dc2bebae24ee7aece5b0fa2eec61cb4cfe989749
Berin <berin@web-vision.de>
2020-11-07 01:45:00 +0100

    Add empty modal template and empty view model

---
+++ ViewModel/MultipleWishlist.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @category  BKozlic
+ * @package   BKozlic\MultipleWishlist
+ * @author    Berin Kozlic - berin.kozlic@gmail.com
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+namespace BKozlic\MultipleWishlist\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class MultipleWishlist implements ArgumentInterface
+{
+
+}
