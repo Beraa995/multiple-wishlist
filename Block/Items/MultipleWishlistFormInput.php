0adf21f4128823fc5f38e4e7140cb56c9076669b
Berin <berin@web-vision.de>
2020-11-11 22:32:17 +0100

    Add input to the wishlist list form

---
+++ Block/Items/MultipleWishlistFormInput.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @category  BKozlic
+ * @package   BKozlic\MultipleWishlist
+ * @author    Berin Kozlic - berin.kozlic@gmail.com
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+namespace BKozlic\MultipleWishlist\Block\Items;
+
+use BKozlic\MultipleWishlist\Api\Data\MultipleWishlistInterface;
+use Magento\Wishlist\Block\Customer\Wishlist\Item\Column;
+
+class MultipleWishlistFormInput extends Column
+{
+    /**
+     * Returns multiple wishlist input for the wishlist form
+     *
+     * @return string
+     */
+    public function getAdditionalHtml()
+    {
+        //@TODO Check if ifconfig in layout xml iw working
+        $paramName = MultipleWishlistInterface::MULTIPLE_WISHLIST_PARAM_NAME;
+        $multipleWishlistId = $this->getRequest()->getParam($paramName);
+
+        return '<input type="hidden" name="' . $paramName . '" value="' . $multipleWishlistId . '">';
+    }
+}
